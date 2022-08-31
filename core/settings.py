b41433f72b1819c677ee867518dfb79052c1efd5
EngFloxy <wumiextra123@gmail.com>
2022-08-31 15:27:09 +0100

    new core

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&91ax!6x2g7c!cw-_3dc#by3svsfaks7fly7tog8ag-ahny+18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.85.131.220', '*']
+ALLOWED_HOSTS = ['54.89.252.130', '*']
 
 
 # Application definition
